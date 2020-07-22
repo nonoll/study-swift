aaf4dc60dabe5876507d218fabc62c6d7c3ec815
nonoll <hkroh@wemakeprice.com>
2020-07-22 17:42:32 +0900

    feat: MusicPlayer - 1

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by we on 22/07/2020.
+//  Copyright © 2020 nonoll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
